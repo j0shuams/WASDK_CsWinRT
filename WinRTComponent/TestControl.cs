cc24d0296c8b4091a2662ed6364af8c7eda956b9
Joshua Larkin <70237359+j0shuams@users.noreply.github.com>
2022-01-24 12:30:13 -0800

    Add project files.

---
+++ WinRTComponent/TestControl.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace WinRTComponent
+{
+    public sealed partial class TestControl : UserControl
+    {
+
+        public static readonly DependencyProperty LabelProperty =
+            DependencyProperty.Register(nameof(Label), typeof(string), typeof(TestControl), new PropertyMetadata(string.Empty));
+
+        public static readonly DependencyProperty BgColorProperty =
+            DependencyProperty.Register(nameof(BackgroundColor), typeof(string), typeof(TestControl), new PropertyMetadata(string.Empty));
+
+        public TestControl()
+        {
+        }
+
+        public string Label
+        {
+            get { return (string)GetValue(LabelProperty); }
+            set { SetValue(LabelProperty, value); }
+        }
+
+        public string BackgroundColor
+        {
+            get { return (string)GetValue(BgColorProperty); }
+            set { SetValue(BgColorProperty, value); }
+        }
+    }
+}
---
+++ WinRTComponent/TestPanel.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.UI.Xaml.Controls;
+using Windows.Foundation;
+
+namespace WinRTComponent
+{
+    public sealed class TestPanel : StackPanel
+    {
+        public bool Food { get; set; }
+
+        public TestPanel() : this(false) { }
+        public TestPanel(bool x) { Food = x; }
+        protected override Size MeasureOverride(Size availableSize)
+        {
+            foreach (var child in Children)
+            {
+                child.Measure(new Size(160, 50));
+            }
+
+            return new Size(330, 500);
+        }
+
+        protected override Size ArrangeOverride(Size finalSize)
+        {
+            int x = 0, y = 0;
+            foreach (var child in Children)
+            {
+                child.Arrange(new Rect(x, y, child.DesiredSize.Width, child.DesiredSize.Height));
+                x = (x + 165) % 330;
+                if (x == 0)
+                {
+                    y += 50;
+                }
+            }
+
+            return new Size(330, 500);
+        }
+    }
+}
